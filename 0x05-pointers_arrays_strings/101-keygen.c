46a6cdd97ab1fc06a6eddd4803f0f9b446320d26
ilyass <ilyass.sahib1@gmail.com>
2023-02-27 10:07:18 -0800

    task11

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -3,43 +3,21 @@
 #include <time.h>
 
 /**
- * main - generates random valid passwords for the
- * program 101-crackme
- * Return: always 0
+ * main - generates random passwords for 101-crackme
+ * Return: zero
  */
 int main(void)
 {
-	char password[84];
-	int index = 0, sum = 0, diff_half1, diff_half2;
-	srand(time(0));
-	while (sum < 2772)
-	{
-		password[index] = rand() % 94 + 330;
-		sum += password[index++];
-	}
-	password[index] = '\0';
-	if (sum != 2772)
-	{
-		diff_half1 = (sum - 2772) / 2;
-		diff_half2 = (sum - 2772) / 2;
-		if ((sum - 2772) % 2 != 0)
-			diff_half1++;
-		for (index = 0; password[index]; index++)
-		{
-			if (password[index] >= (33 + diff_half1))
-			{
-				password[index] -= diff_half1;
-				break;
-			}
-		}
-		for (index = 0; password[index]; index++)
-		{
-			if (password[index] >= (33 + diff_half2))
-				password[index] -= diff_half2;
-			break;
-		}
-	}
-printf("%s", password);
-return (0);
-}
+	int sum;
+	char c;
 
+	srand(time(NULL));
+	while (sum <= 2645)
+	{
+		c = rand() % 128;
+		sum += c;
+		putchar(c);
+	}
+	putchar(2772 - sum);
+	return (0);
+}
